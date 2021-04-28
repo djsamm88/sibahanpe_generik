97060ac3417427d8bbc497b308eb0e7051cbea70
medantechno <admin@medantechno.com>
2021-04-28 13:48:17 +0700

    err

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -60,7 +60,7 @@ class Login extends CI_Controller {
 		
 		if($this->session->userdata('random')!=$this->input->post('captcha'))
 		{
-			die("5");
+			die("1");
 		}
 
 		$user = $this->gas($this->input->post('user_admin'));
